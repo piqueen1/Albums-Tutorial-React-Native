2cc918bdca6666d0717d525864c77869f2950645
Finn Thye <piqueen@gmail.com>
2017-01-05 08:49:36 -0700

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,17 @@
+// iOS code goes here
+
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'I love Amelie'} />
+    <AlbumList />
+  </View>
+
+);
+  
+
+AppRegistry.registerComponent('albums', () => App);
